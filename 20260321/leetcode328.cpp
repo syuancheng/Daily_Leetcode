66b2a2ff15c94565bac811b8dbd1c4ae2657c03e
siyuancheng <syuan.cheng21@gmail.com>
2026-03-21 17:52:39 +0800

    328

---
+++ 20260321/leetcode328.cpp
@@ -0,0 +1,43 @@
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+  ListNode *oddEvenList(ListNode *head) {
+    if (head == nullptr || head->next == nullptr) {
+      return head;
+    }
+    ListNode *h = new ListNode(0);
+
+    ListNode *r = h, *pre = head, *ou = head->next, *tail, *q;
+
+    while (ou != nullptr) {
+      q = ou->next;
+      if (q == nullptr) {
+        tail = pre;
+        r->next = ou;
+        r = r->next;
+        pre->next = nullptr;
+        continue;
+      }
+      r->next = ou;
+      r = r->next;
+      ou->next = nullptr;
+
+      pre->next = q;
+
+      ou = q->next;
+      pre = q;
+      tail = q;
+    }
+
+    tail->next = h->next;
+
+    return head;
+  }
+};
