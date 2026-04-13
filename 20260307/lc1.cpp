540acdacada234fa135a12a9f992f1d745ca8fcc
siyuancheng <syuan.cheng21@gmail.com>
2026-04-13 16:39:10 +0800

    update

--- 20260307/lc1.cpp
+++ 20260307/lc1.cpp
@@ -5,6 +5,22 @@
 
 using namespace std;
 
+/**
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值
+target  的那 两个 整数，并返回它们的数组下标。
+你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+你可以按任意顺序返回答案。
+示例 1：
+输入：nums = [2,7,11,15], target = 9
+输出：[0,1]
+解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+示例 2：
+输入：nums = [3,2,4], target = 6
+输出：[1,2]
+示例 3：
+输入：nums = [3,3], target = 6
+输出：[0,1]
+*/
 class Solution {
 public:
   vector<int> twoSum(vector<int> &nums, int target) {
@@ -27,7 +43,7 @@ std::vector<int> twoSum(std::vector<int> &nums, int target) {
   for (int i = 0; i < nums.size(); i++) {
     auto it = hashtable.find(target - nums[i]);
     if (it != hashtable.end()) {
-        return {it->second, i};
+      return {it->second, i};
     }
     hashtable[nums[i]] = i;
   }
