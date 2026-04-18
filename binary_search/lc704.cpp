1dc0b7f4fc1d9e6250b62cf93ea4b2e0b10745ae
siyuancheng <syuan.cheng21@gmail.com>
2026-04-18 14:16:52 +0800

    704

---
+++ binary_search/lc704.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+/**
+704. 二分查找
+*/
+
+using namespace std;
+
+class Solution {
+public:
+  int search(vector<int> &nums, int target) {
+    int l = 0, r = nums.size() - 1;
+    while (l <= r) {
+      int mid = (r - l) / 2 + l;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] < target) {
+        l = mid + 1;
+      } else if (nums[mid] > target) {
+        r = mid - 1;
+      }
+    }
+    return -1;
+  }
+};
