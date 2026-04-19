42fc14ed30565fb8cb3d76551393896bf8cef389
siyuancheng <syuan.cheng21@gmail.com>
2026-04-19 17:01:15 +0800

    update

--- 20260330/lc15.cpp
+++ 20260330/lc15.cpp
@@ -2,6 +2,10 @@
 #include <cstddef>
 #include <vector>
 
+/**
+15. 三数之和
+*/
+
 using namespace std;
 
 class Solution {
@@ -10,12 +14,12 @@ public:
     vector<vector<int>> ans;
     stable_sort(nums.begin(), nums.end());
 
-    for (size_t i = 0; i < nums.size(); i++) {
+    for (int i = 0; i < nums.size(); i++) {
       if (i > 0 && nums[i] == nums[i - 1]) {
         continue;
       }
-      int l = i + 1, r = nums.size() - 1;
       int target = -nums[i];
+      int l = i + 1, r = nums.size() - 1;
       while (l < r) {
         int sum = nums[l] + nums[r];
         if (sum == target) {
@@ -25,7 +29,7 @@ public:
           while (l < r && nums[l] == nums[l - 1]) {
             l++;
           }
-          while (l < r && nums[r] == nums[r + 1]) {
+          while (r > l && nums[r] == nums[r + 1]) {
             r--;
           }
         } else if (sum < target) {
@@ -35,6 +39,7 @@ public:
         }
       }
     }
+
     return ans;
   }
 };
