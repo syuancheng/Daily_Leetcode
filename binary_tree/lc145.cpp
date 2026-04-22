641010eb156aa5b1f9886bcd27e0290ae8e80188
siyuancheng <syuan.cheng21@gmail.com>
2026-04-22 08:43:15 +0800

    upate145

--- binary_tree/lc145.cpp
+++ binary_tree/lc145.cpp
@@ -1,5 +1,7 @@
 #include "tree.h"
+#include <algorithm>
 #include <cstddef>
+#include <stack>
 #include <vector>
 using namespace std;
 
@@ -13,7 +15,6 @@ public:
       return;
     }
     postorderOrder(node->left, res);
-
     postorderOrder(node->right, res);
     res.emplace_back(node->val);
   }
@@ -24,5 +25,29 @@ public:
     }
     postorderOrder(root, ans);
     return ans;
+  }
+
+  // #2, 左，右，根 -》 根  右 左
+  void postorderOrder2(TreeNode *node, vector<int> &res) {
+    if (node == nullptr) {
+      return;
+    }
+    stack<TreeNode *> sk;
+    sk.push(node);
+
+    while (!sk.empty()) {
+      TreeNode *curr = sk.top();
+      sk.pop();
+      res.push_back(curr->val);
+
+      if (curr->left) {
+        sk.push(curr->left);
+      }
+      if (curr->right) {
+        sk.push(curr->right);
+      }
+    }
+
+    reverse(res.begin(), res.end());
   }
 };
