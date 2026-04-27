c60bb59098d316103de952ff96c94e035f549560
siyuancheng <syuan.cheng21@gmail.com>
2026-04-27 14:36:24 +0800

    sort

--- basics/lc912_sort_an_array.cpp
+++ basics/lc912_sort_an_array.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstdlib>
 #include <utility>
 #include <vector>
@@ -6,22 +7,10 @@ using namespace std;
 
 class Solution {
 public:
-  vector<int> sortArray(vector<int> &nums) {
-    int n = nums.size();
-    for (int i = 0; i < n; i++) {
-      int minIdx = i;
-      for (int j = i + 1; j < n; j++) {
-        if (nums[j] < nums[minIdx]) {
-          minIdx = j;
-        }
-      }
-      swap(nums[i], nums[minIdx]);
-    }
-
-    return nums;
-  }
+  vector<int> sortArray(vector<int> &nums) {}
 };
 
+// 1.快速排序
 int partition(vector<int> &nums, int left, int right) {
   int l = left, r = right;
 
@@ -42,13 +31,63 @@ int partition(vector<int> &nums, int left, int right) {
 }
 
 void quickSort(vector<int> &nums, int left, int right) {
-  if (left < right) {
-    int mid = partition(nums, left, right);
-    quickSort(nums, left, mid - 1);
-    quickSort(nums, mid+1, right);
+  if (left >= right) {
+    return;
+  }
+  int mid = partition(nums, left, right);
+  quickSort(nums, left, mid - 1);
+  quickSort(nums, mid + 1, right);
+}
+
+// 2.选择排序，双指针。内循环选择最小的和外循环交换
+void selectionSort(vector<int> &nums) {
+  int n = nums.size();
+
+  for (int i = 0; i < n - 1; i++) {
+    int k = i;
+    for (int j = i + 1; j < n; j++) {
+      if (nums[j] < nums[k]) {
+        k = j;
+      }
+    }
+    swap(nums[i], nums[k]);
   }
 }
 
+// 3. 归并排序
+void merge(vector<int> &nums, int left, int mid, int right) {
+  vector<int> tmp(right - left + 1);
+  int i = left, j = mid + 1, k = 0;
+  while (i <= mid && j <= right) {
+    if (nums[i] <= nums[j]) {
+      tmp[k++] = nums[i++];
 
+    } else {
+      tmp[k++] = nums[j++];
+    }
+  }
+  while (i <= mid) {
+    tmp[k++] = nums[i++];
+  }
+  while (j <= right) {
+    tmp[k++] = nums[j++];
+  }
+
+  for (k = 0; k < tmp.size(); k++) {
+    nums[left + k] = tmp[k];
+  }
+}
+
+void mergeSort(vector<int> &nums, int left, int right) {
+  if (left >= right) {
+    return;
+  }
+  int mid = left + (right - left) / 2;
+  mergeSort(nums, left, mid);
+  mergeSort(nums, mid + 1, right);
+
+  // 合并阶段
+  merge(nums, left, mid, right);
+}
 
 int main() { return 0; }
