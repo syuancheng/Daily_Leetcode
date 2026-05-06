f1187d28ee7d345ee8fe3c3a9e70ccc883aab4f7
siyuancheng <syuan.cheng21@gmail.com>
2026-05-06 16:56:26 +0800

    update 704

--- basics/lc704_binary_search.cpp
+++ basics/lc704_binary_search.cpp
@@ -6,17 +6,16 @@ class Solution {
 public:
   int search(vector<int> &nums, int target) {
     int l = 0, r = nums.size() - 1;
-
     while (l <= r) {
       int mid = (r - l) / 2 + l;
-      int num = nums[mid];
-
-      if (num == target) {
+      if (nums[mid] == target) {
         return mid;
-      } else if (num < target) {
-        l = mid + 1;
+      }
+
+      if (nums[mid] < target) {
+        l = mid+1;
       } else {
-        r = mid - 1;
+        r = mid -1;
       }
     }
     return -1;
