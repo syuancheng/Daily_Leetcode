ca008f6aad940a410194be29d4de27e534d35c29
siyuancheng <syuan.cheng21@gmail.com>
2026-05-09 16:46:31 +0800

    xxx

--- basics/test.cpp
+++ basics/test.cpp
@@ -1,82 +1,39 @@
 
 
-/**
-给你一个字符串 s，请你将 s 分割成一些 子串，使每个子串都是 回文串 。返回 s
-所有可能的分割方案。
-
-示例 1：
-输入：s = "aab"
-输出：[["a","a","b"],["aa","b"]]
-
-示例 2：
-输入：s = "a"
-输出：[["a"]]
-提示：
-1 <= s.length <= 16
-s 仅由小写英文字母组成
-*/
-
-/**
-abbab
-*/
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <mutex>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
-using namespace std;
+struct Node {
+  std::string name;
+  std::vector<std::string> input_nodes;
+  int count = 0;
 
-class Solution {
-private:
-  vector<vector<string>> result;
-  vector<string> path;
-  vector<vector<bool>> isPalindrome;
-
-  void backtracking(const string &s, int startIndex) {
-    // 如果起始位置大于等于字符串长度，说明找到了一组方案
-    if (startIndex >= s.size()) {
-      result.push_back(path);
-      return;
-    }
-
-    for (int i = startIndex; i < s.size(); i++) {
-      // 使用预处理好的 DP 数组判断回文
-      if (isPalindrome[startIndex][i]) {
-        string str = s.substr(startIndex, i - startIndex + 1);
-        path.push_back(str);
-        backtracking(s, i + 1); // 递归寻找剩余子串
-        path.pop_back();        // 回溯，撤销处理结果
-      }
-    }
-  }
-
-public:
-  vector<vector<string>> partition(string s) {
-    int n = s.size();
-    // 1. 预处理回文状态 DP 数组
-    isPalindrome.assign(n, vector<bool>(n, false));
-    for (int j = 0; j < n; j++) {
-      for (int i = 0; i <= j; i++) {
-        if (s[i] == s[j] && (j - i <= 2 || isPalindrome[i + 1][j - 1])) {
-          isPalindrome[i][j] = true;
-        }
-      }
-    }
-
-    backtracking(s, 0);
-    return result;
-  }
+  Node(std::string &node_name, std::vector<std::string> &nodes)
+      : name(node_name), input_nodes(nodes), count(nodes.size()) {}
 };
 
+int main() {
 
-void backTracking() {
-  if (/**终止条件 */) {
-    // 收集结果
-    return;
+  std::vector<std::string> input_nodes = {"node1", "node2"};
+
+  std::shared_ptr<Node> node1 = std::make_shared<Node>("node1");
+  std::shared_ptr<Node> node2 = std::make_shared<Node>("node2");
+  std::shared_ptr<Node> node3 = std::make_shared<Node>("node3", input_nodes);
+
+  std::vector<std::shared_ptr<Node>> nodes = {node1, node2, node3};
+
+  //执行所有入读为0的node， 所有入度不为0的node 入读减一
+  for (auto& node : nodes) {
+    if (node->count != 0) {
+        node->count--;
+        continue;
+    }
+    std::cout << "run " << node->name << std::endl;
   }
-  // 单层搜索逻辑
-  for (/**集合里的每一个元素，也可以是叶子节点的个数 */) {
-    // 处理节点
-    // 递归函数
-    // 回溯操作，撤销梳理节点的情况
-  }
-  return;
+  
 }
