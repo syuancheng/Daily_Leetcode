df3a7cbdbb0a24b0d27ec85f1f4b2f5f5b11d121
siyuancheng <syuan.cheng21@gmail.com>
2026-05-10 17:36:45 +0800

    update lc111

---
+++ bfs/lc207_can_finish.cpp
@@ -0,0 +1,38 @@
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  bool canFinish(int numCourses, vector<vector<int>> &prerequisites) {
+    vector<vector<int>> graph(numCourses);
+    vector<int> inDegree(numCourses, 0);
+
+    for (auto &p : prerequisites) {
+      graph[p[1]].push_back(p[0]); // 学完p1才能学p0
+      inDegree[p[0]]++;
+    }
+
+    queue<int> q;
+    for (int i = 0; i < numCourses; i++) {
+      if (inDegree[i] == 0) {
+        q.push(i);
+      }
+    }
+
+    int finished = 0;
+    while (!q.empty()) {
+      int node = q.front();
+      q.pop();
+      finished++;
+
+      for (int next : graph[node]) {
+        if (--inDegree[next] == 0) {
+          q.push(next);
+        }
+      }
+    }
+    return finished == numCourses;
+  }
+};
