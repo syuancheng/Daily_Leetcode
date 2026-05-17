04f6cd45ed6edd9f6dc67c409e4635497f3ea0e0
siyuancheng <syuan.cheng21@gmail.com>
2026-05-17 15:01:05 +0800

    lc70

--- basics/lc70_climp_staris.cpp
+++ basics/lc70_climp_staris.cpp
@@ -1,9 +1,18 @@
+#include <vector>
 class Solution {
 public:
   int climbStairs(int n) {
     if (n == 1 || n == 2) {
       return n;
     }
-    return climbStairs(n - 1) + climbStairs(n - 2);
+    std::vector<int> dp(n + 1);
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+      dp[i] = dp[i-1] +dp[i-2];
+    }
+
+    return dp[n];
   }
 };
