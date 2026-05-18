1501be755904598bf149c08977bbaa337df40bf3
siyuancheng <syuan.cheng21@gmail.com>
2026-05-18 12:27:15 +0800

    update

--- basics/lc912_sort_an_array/heap_sort.cpp
+++ basics/lc912_sort_an_array/heap_sort.cpp
@@ -1,3 +1,4 @@
+#include <utility>
 #include <vector>
 
 using namespace std;
@@ -11,11 +12,34 @@ public:
 
 private:
   void heapSort(vector<int> &nums) {
-    // TODO: implement heap sort
+    int n = nums.size();
+    for (int i = n / 2 - 1; i >= 0; i--) {
+      heapify(nums, n, i);
+    }
+
+    for (int i = n - 1; i > 0; i--) {
+      std::swap(nums[0], nums[i]);
+
+      heapify(nums, i, 0);
+    }
   }
 
   void heapify(vector<int> &nums, int heapSize, int root) {
-    // TODO: implement heapify helper
+    int largest = root;
+    int leftChild = root * 2 + 1;
+    int rightChind = root * 2 + 2;
+
+    if (leftChild < heapSize && nums[leftChild] > nums[largest]) {
+      largest = leftChild;
+    }
+    if (rightChind < heapSize && nums[rightChind] > nums[largest]) {
+      largest = rightChind;
+    }
+
+    if (largest != root) {
+      swap(nums[largest], nums[root]);
+      heapify(nums, heapSize, largest);
+    }
   }
 };
 
