29d1c17b60fb838ced09c88753e6920affe92168
siyuancheng <syuan.cheng21@gmail.com>
2026-05-18 10:56:20 +0800

    update

--- basics/lc912_sort_an_array.cpp
+++ basics/lc912_sort_an_array.cpp
@@ -1,3 +1,4 @@
+#include <utility>
 #include <vector>
 
 using namespace std;
@@ -10,20 +11,43 @@ public:
   }
 
   // 1. 冒泡排序
-  vector<int> bubbleSort(vector<int> &nums) {
+  void bubbleSort(vector<int> &nums) {
     // TODO: implement bubble sort
-    return nums;
+    int size = nums.size();
+    for (int i = size - 1; i > 0; i--) {
+      for (int j = 0; j < i; j++) {
+        if (nums[j] > nums[j + 1]) {
+          swap(nums[j], nums[j + 1]);
+        }
+      }
+    }
   }
 
   // 2. 选择排序
-  vector<int> selectionSort(vector<int> &nums) {
+  void selectionSort(vector<int> &nums) {
     // TODO: implement selection sort
-    return nums;
+    for (int i = 0; i < nums.size(); i++) {
+      int k = i;
+      for (int j = i + 1; j < nums.size(); j++) {
+        if (nums[j] < nums[k]) {
+          k = j;
+        }
+      }
+      swap(nums[i], nums[k]);
+    }
   }
 
   // 3. 插入排序
   vector<int> insertionSort(vector<int> &nums) {
     // TODO: implement insertion sort
+    for (int i = 1; i < nums.size(); i++) {
+      int base = nums[i], j = i - 1; // 这里要认为i以前都排好序了。
+      while (j >= 0 && nums[j] > base) {
+        nums[j + 1] = nums[j];
+        j--;
+      }
+      nums[j + 1] = base;
+    }
     return nums;
   }
 
@@ -42,6 +66,7 @@ public:
   // 6. 快速排序
   vector<int> quickSort(vector<int> &nums) {
     // TODO: implement quick sort
+    quickSort(nums, 0, nums.size() - 1);
     return nums;
   }
 
@@ -80,11 +105,32 @@ private:
 
   void quickSort(vector<int> &nums, int left, int right) {
     // TODO: implement quick sort recursion
+    if (left >= right) {
+      return;
+    }
+    int mid = partition(nums, left, right);
+    quickSort(nums, left, mid - 1);
+    quickSort(nums, mid + 1, right);
   }
 
   int partition(vector<int> &nums, int left, int right) {
     // TODO: implement partition helper
-    return left;
+    int base = nums[left], l = left, r = right;
+    while (l < r) {
+      while (l < r && nums[r] >= base) {
+        r--;
+      }
+
+      while (l < r && nums[l] <= base) {
+        l++;
+      }
+      if (l < r) {
+        swap(nums[l], nums[r]);
+      }
+    }
+    swap(nums[left], nums[l]);
+
+    return l;
   }
 
   void heapify(vector<int> &nums, int heapSize, int root) {
