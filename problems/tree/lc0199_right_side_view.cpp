ce83d047bb298979dfb26adb0b5c4081b2663a94
siyuancheng <syuan.cheng21@gmail.com>
2026-05-26 22:31:59 +0800

    remove

--- problems/tree/lc0199_right_side_view.cpp
+++ problems/tree/lc0199_right_side_view.cpp
@@ -8,38 +8,5 @@ class Solution {
 
 public:
   vector<int> rightSideView(TreeNode *root) {
-    if (root == nullptr) {
-      return {};
-    }
-
-    vector<int> result;
-
-    queue<TreeNode *> q;
-    q.push(root);
-
-    while (!q.empty()) {
-      int size = q.size();
-
-      vector<int> level;
-
-      for (int i = 0; i < size; ++i) {
-        auto node = q.front();
-        q.pop();
-
-        level.push_back(node->val);
-
-        if (node->left) {
-          q.push(node->left);
-        }
-        if (node->right) {
-          q.push(node->right);
-        }
-      }
-      if (!level.empty()) {
-        result.push_back(level.back());
-      }
-    }
-
-    return result;
   }
 };
