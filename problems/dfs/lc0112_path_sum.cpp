70b8d92d46aa685c90cd33be0a212b5b1240770d
siyuancheng <syuan.cheng21@gmail.com>
2026-05-30 16:47:27 +0800

    review 112

--- problems/dfs/lc0112_path_sum.cpp
+++ problems/dfs/lc0112_path_sum.cpp
@@ -4,15 +4,6 @@
 class Solution {
 public:
   bool hasPathSum(TreeNode *root, int targetSum) {
-    if (root == nullptr) {
-      return false;
-    }
-
-    if (root->left == nullptr && root->right == nullptr) {
-      return root->val == targetSum;
-    }
-    int remain = targetSum - root->val;
-
-    return hasPathSum(root->left, remain) || hasPathSum(root->right, remain);
+    
   }
 };
