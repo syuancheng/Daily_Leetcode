740ca0a9eb759ea4396146fc9971cea61f9f6434
siyuancheng <syuan.cheng21@gmail.com>
2026-05-30 16:41:04 +0800

    update

--- problems/dfs/lc0113_path_sum_ii.cpp
+++ problems/dfs/lc0113_path_sum_ii.cpp
@@ -4,7 +4,11 @@
 using namespace std;
 
 class Solution {
+private:
+  vector<vector<int>> result;
+
+  int target{0};
+
 public:
-  vector<vector<int>> pathSum(TreeNode *root, int targetSum) {
-  }
+  vector<vector<int>> pathSum(TreeNode *root, int targetSum) {}
 };
