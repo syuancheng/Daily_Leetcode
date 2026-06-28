1d640f6f9dd04d68bdcb17300d80530b66422463
siyuancheng <syuan.cheng21@gmail.com>
2026-06-28 14:17:00 +0800

    temp my array list

---
+++ my_datastruct/array_list.h
@@ -0,0 +1,40 @@
+
+
+template <typename E> class MyArrayList {
+private:
+  E *data;
+
+  int size;
+  int cap;
+
+  static const int INIT_CAP = 1;
+
+public:
+  MyArrayList() {
+    this->data = new E[INIT_CAP];
+    this->size = 0;
+    this->cap = INIT_CAP;
+  }
+
+  MyArrayList(int initCapacity) {
+    this->data = new E[initCapacity];
+    this->size = 0;
+    this->cap = initCapacity;
+  }
+
+  void addLast(E e) {
+    if (size == cap) {
+      // resize();
+    }
+    data[size] = e;
+    size++;
+  }
+
+  void add(int index, E e) {
+    
+  }
+
+  bool isElementIndex(int index) { return index >= 0 && index < size; }
+
+  bool isPositionIndex(int index) { return index >= 0 && index <= size; }
+};
