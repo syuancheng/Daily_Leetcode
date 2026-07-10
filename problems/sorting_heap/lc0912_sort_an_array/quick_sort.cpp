33bff4d64516d07c4b8316b3cb979563f5f08289
siyuancheng <syuan.cheng21@gmail.com>
2026-07-10 13:43:09 +0800

    review quick sort

--- problems/sorting_heap/lc0912_sort_an_array/quick_sort.cpp
+++ problems/sorting_heap/lc0912_sort_an_array/quick_sort.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <locale>
 #include <utility>
 #include <vector>
 
@@ -6,7 +7,38 @@ using namespace std;
 
 class Solution {
 public:
-  vector<int> sortArray(vector<int> &nums) {}
+  vector<int> sortArray(vector<int> &nums) {
+    quickSort(nums, 0, nums.size() - 1);
+  }
+
+private:
+  void quickSort(vector<int> &nums, int start, int end) {
+    if (start >= end) {
+      return;
+    }
+    int mid = prorition(nums, start, end);
+    quickSort(nums, start, mid - 1);
+    quickSort(nums, mid + 1, end);
+  }
+
+  int prorition(vector<int> &nums, int start, int end) {
+    int l = start, r = end;
+    int base = nums[start];
+
+    while (l < r) {
+      while (l < r && nums[r] >= base) {
+        r--;
+      }
+      while (l < r && nums[l] <= base) {
+        l++;
+      }
+      if (l < r) {
+        swap(nums[l], nums[r]);
+      }
+    }
+    swap(nums[l], base);
+    return l
+  }
 };
 
 int main() {}
