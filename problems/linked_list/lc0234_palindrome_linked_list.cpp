96796bbd644df18153e061766ea0c7234f2d1541
siyuancheng <syuan.cheng21@gmail.com>
2026-07-11 21:55:23 +0800

    review 234

--- problems/linked_list/lc0234_palindrome_linked_list.cpp
+++ problems/linked_list/lc0234_palindrome_linked_list.cpp
@@ -26,3 +26,45 @@ public:
     left = left->next;
   }
 };
+
+class Solution2 {
+public:
+  bool isPalindrome(ListNode *head) {
+    ListNode *slow, *fast;
+    slow = fast = head;
+    while (fast != nullptr && fast->next != nullptr) {
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+
+    if (fast != nullptr) {
+      slow = slow->next;
+    }
+
+    ListNode *left = head;
+    ListNode *right = reverse(slow);
+    while (right != nullptr) {
+      if (left->val != right->val) {
+        return false;
+      }
+      right = right->next;
+      left = left->next;
+    }
+
+    return true;
+  }
+
+  ListNode *reverse(ListNode *head) {
+    ListNode *pre = nullptr, *cur = head;
+    while (cur != nullptr) {
+      ListNode *next = cur->next;
+
+      cur->next = pre;
+      pre = cur;
+
+      cur = next;
+    }
+
+    return cur;
+  }
+};
