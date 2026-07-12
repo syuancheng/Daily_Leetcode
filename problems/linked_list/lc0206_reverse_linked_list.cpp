7fb69d0e9aeb5b780864b321180224233702a801
siyuancheng <syuan.cheng21@gmail.com>
2026-07-12 08:01:44 +0800

    review 206

--- problems/linked_list/lc0206_reverse_linked_list.cpp
+++ problems/linked_list/lc0206_reverse_linked_list.cpp
@@ -3,5 +3,35 @@
 class Solution {
 public:
   ListNode *reverseList(ListNode *head) {
+    if (head == nullptr || head->next == nullptr) {
+      return head;
+    }
+
+    ListNode *pre = nullptr, *cur = head, *next = head->next;
+
+    while (cur != nullptr) {
+      cur->next = pre;
+      pre = cur;
+      cur = next;
+      if (next != nullptr) {
+        next = next->next;
+      }
+    }
+    return pre;
+  }
+};
+
+
+class Solution2 {
+public:
+  ListNode *reverseList(ListNode *head) {
+    if (head == nullptr || head->next == nullptr) {
+      return head;
+    }
+
+    ListNode *last = reverseList(head->next);
+    head->next->next = head;
+    head->next = nullptr;
+    return last;
   }
 };
