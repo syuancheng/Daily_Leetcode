922f142f60eb25055f683882312dd0ab87714869
siyuancheng <syuan.cheng21@gmail.com>
2026-07-13 19:36:05 +0800

    review 76

--- problems/sliding_window/lc0076_minimum_window_substring.cpp
+++ problems/sliding_window/lc0076_minimum_window_substring.cpp
@@ -1,7 +1,6 @@
 #include <climits>
 #include <string>
 #include <unordered_map>
-#include <vector>
 
 using namespace std;
 
@@ -11,11 +10,42 @@ using namespace std;
 
 class Solution {
 public:
-  bool check(vector<int> &flag, vector<int> &test) {
-  }
-
-  // s = abc, t=a
-
   string minWindow(string s, string t) {
+    unordered_map<char, int> need, window;
+    for (char c : t) {
+      need[c]++;
+    }
+
+    int left = 0, right = 0;
+    int valid = 0;
+
+    int start = 0, len = INT_MAX;
+    while (right < s.size()) {
+      char c = s[right];
+      right++;
+
+      if (need.count(c)) {
+        window[c]++;
+        if (need[c] == window[c]) {
+          valid++;
+        }
+      }
+
+      while (valid == need.size()) {
+        if (right - left < len) {
+          start = left;
+          len = right - left;
+        }
+        char d = s[left];
+        left++;
+        if (need.count(d)) {
+          if (window[d] == need[d]) {
+            valid--;
+          }
+          window[d]--;
+        }
+      }
+    }
+    return len == INT_MAX ? "" : s.substr(start, len);
   }
 };
