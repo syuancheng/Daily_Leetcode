9c97159095f122921cc3b4807dc79acb1c80c7e7
siyuancheng <syuan.cheng21@gmail.com>
2026-07-13 15:08:10 +0800

    27

--- problems/array_hashing/lc0027_remove_element.cpp
+++ problems/array_hashing/lc0027_remove_element.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
 #include <vector>
 
-int removeElement(std::vector<int> &nums, int val) {
-}
-
-int removeElementv2(std::vector<int> &nums, int val) {
-}
+using namespace std;
+class Solution {
+public:
+  int removeElement(vector<int> &nums, int val) {
+    int slow = 0, fast = 0;
+    while (fast < nums.size()) {
+      if (nums[fast] != val) {
+        nums[slow] = nums[fast];
+        slow++;
+      }
+      fast++;
+    }
+    return slow;//这里不需要+1， 因为slow索引是先复制，然后才++
+  }
+};
