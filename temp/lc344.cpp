64bfe84f797038563f6442eb9deba84dc4af7688
siyuancheng <syuan.cheng21@gmail.com>
2026-07-13 15:25:27 +0800

    new 344

---
+++ temp/lc344.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  void reverseString(vector<char> &s) {
+    int left = 0, right = s.size() - 1;
+    while (left < right) {
+      char temp = s[left];
+
+      s[left] = s[right];
+      s[right] = temp;
+      left++;
+      right--;
+    }
+  }
+};
