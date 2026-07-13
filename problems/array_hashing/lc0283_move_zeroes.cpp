d6ae4b7b5b9b7d5cafbdfebd33d3e2b552e97e3d
siyuancheng <syuan.cheng21@gmail.com>
2026-07-13 15:13:53 +0800

    review 283

--- problems/array_hashing/lc0283_move_zeroes.cpp
+++ problems/array_hashing/lc0283_move_zeroes.cpp
@@ -6,5 +6,22 @@ using namespace std;
 class Solution {
 public:
   void moveZeroes(vector<int> &nums) {
+    int i = removeElement(nums, 0);
+    for (; i < nums.size(); i++) {
+      nums[i] = 0;
+    }
+  }
+
+  // 双指针技巧，复用 [27. 移除元素] 的解法。
+  int removeElement(vector<int> &nums, int val) {
+    int slow = 0, fast = 0;
+    while (fast < nums.size()) {
+      if (nums[fast] != val) {
+        nums[slow] = nums[fast];
+        slow++;
+      }
+      fast++;
+    }
+    return slow;
   }
 };
