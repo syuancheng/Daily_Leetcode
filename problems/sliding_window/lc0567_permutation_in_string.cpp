fb436894131b932801bed065ce28e361e2a57fc4
siyuancheng <syuan.cheng21@gmail.com>
2026-07-13 20:04:32 +0800

    review 567

--- problems/sliding_window/lc0567_permutation_in_string.cpp
+++ problems/sliding_window/lc0567_permutation_in_string.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
@@ -10,5 +11,39 @@ using namespace std;
 class Solution {
 public:
   bool checkInclusion(string s1, string s2) {
+    unordered_map<char, int> need, window;
+    for (char c : s1) {
+      need[c]++;
+    }
+
+    int left = 0, right = 0;
+    int valid = 0;
+
+    while (right < s2.size()) {
+      char c = s2[right];
+      right++;
+
+      if (need.count(c)) {
+        window[c]++;
+        if (need[c] == window[c]) {
+          valid++;
+        }
+      }
+
+      while (right - left >= s1.size()) {
+        if (valid == need.size()) {
+          return true;
+        }
+        char d = s2[left];
+        left++;
+        if (need.count(d)) {
+          if (need[d] == window[d]) {
+            valid--;
+          }
+          window[d]--;
+        }
+      }
+    }
+    return false;
   }
 };
