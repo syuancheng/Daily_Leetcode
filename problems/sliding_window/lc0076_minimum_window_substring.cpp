73ee64b7d183112c9a66fcea322410bfaf5a4d42
siyuancheng <syuan.cheng21@gmail.com>
2026-07-14 09:30:46 +0800

    review 76

--- problems/sliding_window/lc0076_minimum_window_substring.cpp
+++ problems/sliding_window/lc0076_minimum_window_substring.cpp
@@ -16,14 +16,12 @@ public:
       need[c]++;
     }
 
-    int left = 0, right = 0;
-    int valid = 0;
-
+    int left = 0, right = 0, valid = 0;
     int start = 0, len = INT_MAX;
+
     while (right < s.size()) {
       char c = s[right];
       right++;
-
       if (need.count(c)) {
         window[c]++;
         if (need[c] == window[c]) {
@@ -36,10 +34,11 @@ public:
           start = left;
           len = right - left;
         }
+
         char d = s[left];
         left++;
         if (need.count(d)) {
-          if (window[d] == need[d]) {
+          if (need[d] == window[d]) {
             valid--;
           }
           window[d]--;
--- problems/sliding_window/lc0438_find_all_anagrams_in_a_string.cpp
+++ problems/sliding_window/lc0438_find_all_anagrams_in_a_string.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
@@ -11,5 +12,40 @@ using namespace std;
 class Solution {
 public:
   vector<int> findAnagrams(string s, string p) {
+    unordered_map<char, int> need;
+    unordered_map<char, int> window;
+
+    for (char c : p) {
+      need[c]++;
+    }
+
+    int left = 0, right = 0;
+    int valid = 0;
+    vector<int> res;
+    while (right < s.size()) {
+      char c = s[right];
+      right++;
+      if (need.count(c)) {
+        window[c]++;
+        if (need[c] == window[c]) {
+          valid++;
+        }
+      }
+
+      while (right - left >= p.size()) {
+        if (valid == need.size()) {
+          res.push_back(left);
+        }
+        char d = s[left];
+        left++;
+        if (need.count(d)) {
+          if (need[d] == window[d]) {
+            valid--;
+          }
+          window[d]--;
+        }
+      }
+    }
+    return res;
   }
 };
