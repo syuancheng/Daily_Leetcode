52101da22c29cbdd912cd97d2cbbc70f3be5586b
siyuancheng <syuan.cheng21@gmail.com>
2026-07-15 10:40:54 +0800

    review quick sort

--- problems/sorting_heap/lc0912_sort_an_array/quick_sort.cpp
+++ problems/sorting_heap/lc0912_sort_an_array/quick_sort.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstdlib>
 #include <locale>
 #include <utility>
@@ -11,34 +12,31 @@ public:
     quickSort(nums, 0, nums.size() - 1);
   }
 
-private:
-  void quickSort(vector<int> &nums, int start, int end) {
-    if (start >= end) {
+  void quickSort(vector<int> &nums, int l, int r) {
+    if (l >= r) {
       return;
     }
-    int mid = prorition(nums, start, end);
-    quickSort(nums, start, mid - 1);
-    quickSort(nums, mid + 1, end);
+    int mid = partition(nums, l, r);
+    quickSort(nums, l, mid - 1);
+    quickSort(nums, mid + 1, r);
   }
 
-  int prorition(vector<int> &nums, int start, int end) {
-    int l = start, r = end;
-    int base = nums[start];
+  int partition(vector<int> &nums, int left, int right) {
+    int l = left, r = right;
+    int base = nums[left];
 
     while (l < r) {
       while (l < r && nums[r] >= base) {
         r--;
       }
-      while (l < r && nums[l] <= base) {
+      while (l < r && nums[l] >= base) {
         l++;
       }
       if (l < r) {
         swap(nums[l], nums[r]);
       }
     }
-    swap(nums[l], base);
-    return l
+    swap(nums[l], nums[left]);
+    return l;
   }
 };
-
-int main() {}
