a818c8dc62e61f991a90287eec5d9d781ecbac2c
siyuancheng <syuan.cheng21@gmail.com>
2026-07-26 00:08:01 +0800

    two way for lc104

--- problems/dfs/lc0104_maximum_depth_of_binary_tree.cpp
+++ problems/dfs/lc0104_maximum_depth_of_binary_tree.cpp
@@ -1,17 +1,50 @@
 #include "../common/nodes.h"
 #include <algorithm>
 
-/**
-recursion formula： depth(root) = max(depth(root.left), depth(root.right)) + 1;
-stop condition: root == 0 ? 0
-*/
-class Solution {
+// 遍历的思路
+class Solution1 {
+private:
+  int res = 0; // max depth
+
+  int depth = 0;
+
 public:
+  int maxDepth(TreeNode *root) {
+    traverse(root);
+    return res;
+  }
+
+  void traverse(TreeNode *root) {
+    if (root == nullptr) {
+      return;
+    }
+
+    depth++;
+    if (root->left == nullptr && root->right == nullptr) {
+      res = std::max(res, depth);
+    }
+    traverse(root->left);
+    traverse(root->right);
+    depth--;
+  }
+};
+
+// 分解问题的思路
+class Solution2 {
+public:
+  // 定义：输入一个节点，返回以该节点为根的二叉树的最大深度
   int maxDepth(TreeNode *root) {
     if (root == nullptr) {
       return 0;
     }
-    return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    // 利用定义，计算左右子树的最大深度
+    int leftMax = maxDepth(root->left);
+    int rightMax = maxDepth(root->right);
+
+    // 根据左右子树的最大深度推出原二叉树的最大深度
+    // 整棵树的最大深度等于左右子树的最大深度取最大值，
+    // 然后再加上根节点自己
+    return 1 + std::max(leftMax, rightMax);
   }
 };
 
